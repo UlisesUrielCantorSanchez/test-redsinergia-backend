94f270b29c785507c344ac3aafe8c9405ba86c5f
Ulises Uriel <ucantor@tecnodalpha.com>
2025-05-23 13:53:57 -0600

    Primer commit

---
+++ src/main/java/com/redsinergia/TestRedsinergiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.redsinergia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRedsinergiaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRedsinergiaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redsinergia/TestRedsinergiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redsinergia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRedsinergiaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
